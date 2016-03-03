6d9054341a83dd9fede8f3c7c1d8407ea2ab5ff9
icdozen <icdozen@gmail.com>
2016-03-03 15:44:04 -0500

    contractor log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @contractor = Contractor.
+                  find_by(email: params[:email])
+                  try(:authenticate, params[:password])
+
+    if @contractor
+      session[:contractor_id] = @contractor.id
+      redirect_to contractors_path
+      #fix this redirect_to
+    else
+      render action: 'new'
+  end
+end
+
 end
