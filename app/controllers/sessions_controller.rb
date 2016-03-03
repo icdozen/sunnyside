851fe3cbb06c2128edbc28779c79225eae767c7f
icdozen <icdozen@gmail.com>
2016-03-03 16:23:10 -0500

    sessions schema

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
+  # def index
+  # @customers = Customer.all
+  # @contractors = Contractor.all
+  # end
+
   def new
   end
 
@@ -7,10 +12,18 @@ class SessionsController < ApplicationController
                   find_by(email: params[:email])
                   try(:authenticate, params[:password])
 
+    @customer = Customer.
+                  find_by(email: params[:email])
+                  try(:authenticate, params[:password])
     if @contractor
       session[:contractor_id] = @contractor.id
       redirect_to contractors_path
       #fix this redirect_to
+
+    elsif @customer
+      session[:customer_id] = @customer.id
+      redirect_to customers_path
+
     else
       render action: 'new'
   end
