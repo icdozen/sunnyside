c129901b9ac6fd332ef60d631c77a6042111d953
Patchx <robert.anderson.fl@gmail.com>
2016-03-06 10:58:14 -0500

    add miles converter to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,14 @@
 class Customer < ActiveRecord::Base
   has_many :searches
+
+  def searchArea(latInput, longInput)
+  	# 1 degree latitude is 69 miles
+  	# 1 degree longitude = cosine (latitude) * 6.172 
+  	# 
+  	milesLat = 69 * latInput
+  	milesLong = cosine(latInput) * 6.172
+  	return [milesLat, milesLong]
+
+  end
+
 end
