c2a95df6f1b0588756c9955ae8d40d3751600924
Patchx <robert.anderson.fl@gmail.com>
2016-03-08 11:50:34 -0500

    added notes to search function, search not implemented yet

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,12 +2,19 @@ class Customer < ActiveRecord::Base
   has_many :searches
   
   def searchResults(latInput, longInput)
-  	# Binary  search the contractor database for contractors within a
-  	# +/- 0.5 degree lat/long zone.
-  	# Note:
-  		# 1 degree latitude = 69 miles
-  		# 1 degree longitude = cosine (latitude) * 6.172 
+    #
+    # -------------- Overview ----------------------------------------
+  	# Linear search the contractor database for contractors 
+    #   within a +/- 0.5 degree lat/long zone.
   	# These results will be sent to the map. Max search area is ~69 miles
+    #
+    # -------------- Notes ------------------------------------------
+      # Linear search is a better choice than Binary here:
+        # Easier. 
+        # Binary would need to sort our data twice (lat & long), slow.
+      # Actual math for conversion to miles (not implemented):
+        # 1 degree latitude = 69 miles
+        # 1 degree longitude = cosine (latitude) * 6.172 
 
   	output = []
   	@contractors.each do |contractor|
