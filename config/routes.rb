2f73cf31991afc4cac3162edf2e97d9791220f30
Patchx <robert.anderson.fl@gmail.com>
2016-03-08 17:52:47 -0500

    change route to search

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
     # get 'products/:id' => 'catalog#view'
-    get 'customers_search' => 'customers#search'
+    get 'customers-search' => 'customers#search'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
