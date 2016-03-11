8fdf380bf145a65cd14a31029e4fb8812f55728b
Patchx <robert.anderson.fl@gmail.com>
2016-03-11 11:04:45 -0500

    updated seeds and customer search

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,21 +1,20 @@
 class Customer < ActiveRecord::Base
   has_many :searches
-  
-  def self.searchResults(customerIn, contractorsIn)
-    #
-    # -------------- Overview ----------------------------------------
-  	# Linear search the contractor database for contractors 
-    #   within a +/- 0.5 degree lat/long zone.
-  	# These results will be sent to the map. Max search area is ~69 miles
-    #
-    # -------------- Notes ------------------------------------------
-      # Linear search is a better choice than Binary here:
-        # Easier. 
-        # Binary would need to sort our data twice (lat & long), slow.
-      # Actual math for conversion to miles (not implemented):
-        # 1 degree latitude = 69 miles
-        # 1 degree longitude = cosine (latitude) * 6.172 
 
+  #
+  # ----- self.searchResults Overview --------
+  # Linear search the contractor database for contractors 
+  #   within a +/- 0.5 degree lat/long zone.
+  # These results will be sent to the map. Max search area is ~69 miles
+  #
+  # -------------- Notes ------------------------------------------
+    # Linear search is a better choice than Binary here:
+      # Easier. 
+      # Binary would need to sort our data twice (lat & long), slow.
+    # Actual math for conversion to miles (not implemented):
+      # 1 degree latitude = 69 miles
+      # 1 degree longitude = cosine (latitude) * 6.172 
+  def self.searchResults(customerIn, contractorsIn)
   	output = []
   	contractorsIn.each do |contractor|
       diffLat = (customerIn.Lat - contractor.Lat).abs
