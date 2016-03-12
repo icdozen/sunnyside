1b199054965b695f7a1635653176c091e824d6cc
icdozen <icdozen@gmail.com>
2016-03-12 13:09:20 -0500

    style part deux

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,14 +1,6 @@
 class Customer < ActiveRecord::Base
   has_many :searches
 
-  def searchArea(latInput, longInput)
-  	# 1 degree latitude is 69 miles
-  	# 1 degree longitude = cosine (latitude) * 6.172 
-  	# 
-  	milesLat = 69 * latInput
-  	milesLong = cosine(latInput) * 69.172
-  	return [milesLat, milesLong]
-
-  end
+ 
 
 end
