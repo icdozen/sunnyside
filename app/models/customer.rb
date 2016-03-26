2e37187ccfb3f25daa0fc8f7faf817934b1f6d70
Patchx <robert.anderson.fl@gmail.com>
2016-03-26 10:03:41 -0400

    markers now update!  Just need to AJAX the table to show the results

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -18,37 +18,4 @@ class Customer < ActiveRecord::Base
     return output
   end
 
-  #
-  # ----- self.searchResults Overview --------
-  # Linear search the contractor database for contractors 
-  #   within a +/- 0.5 degree lat/long zone.
-  # These results will be sent to the map. Max search area is ~69 miles
-  #
-  # -------------- Notes ------------------------------------------
-    # Linear search is a better choice than Binary here:
-      # Easier. 
-      # Binary would need to sort our data twice (lat & long), slow.
-    # Actual math for conversion to miles (not implemented):
-      # 1 degree latitude = 69 miles
-      # 1 degree longitude = cosine (latitude) * 6.172 
-  def self.searchResults(customerIn, contractorsIn)
-  	output = []
-  	contractorsIn.each do |contractor|
-      diffLat = (customerIn.Lat - contractor.Lat).abs
-      diffLng = (customerIn.Lan - contractor.Lng).abs
-      if (diffLat <= 0.37) && (diffLng <= 0.37) # radius ~50 miles
-    		output.append({
-          name: contractor.name,
-          address: contractor.address,
-          scope: contractor.scope,
-          phone: contractor.phone,
-          email: contractor.email,
-          Lat: contractor.Lat,
-          Lng: contractor.Lng
-        })
-      end
-  	end
-  	return output
-  end
-
 end
